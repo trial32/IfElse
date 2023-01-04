82b4a76c40bbc35d2d4e1411668891a7e2363fea
icmosoftware <ozendeniz@gmail.com>
2023-01-04 11:06:20 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Program başladı..");
+
+int time =DateTime.Now.Hour;
+
+if (time >= 6 && time < 12)
+    Console.WriteLine("Günaydın");
+else if (time <= 18)
+    Console.WriteLine("İyi günler");
+else
+    Console.WriteLine("İyi geceler");
+
+string sonuc = time >= 6 && time < 12 ? "Günaydın" : time <= 18 ? "İyi günler" : "İyi geceler";
+
+Console.WriteLine(sonuc);
